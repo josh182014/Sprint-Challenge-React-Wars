6506b2a931f17cf2660c73948bf1f024300e5c3b
josh182014 <josh182014@gmail.com>
2019-04-26 11:40:25 -0600

    cleaned up code a little

--- starwars/src/components/StarWars.js
+++ starwars/src/components/StarWars.js
@@ -3,13 +3,11 @@ import Character from './Character'
 
 function StarWars(props) {
     if (props.starwarsChars.length > 1) {
-
         return (
             props.starwarsChars.map(character => (
                 <Character character={character} key={character.created}/>
             )
-        )
-        )
+        ))
     }
     else return 'Loading I guess? IDK'
 }
