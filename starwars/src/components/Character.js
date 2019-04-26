a966370423889712ca56d1a3e72848523782f64f
josh182014 <josh182014@gmail.com>
2019-04-26 13:26:37 -0600

    more info button only toggles the character you are clicking

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -3,16 +3,19 @@ import React from 'react'
 function Character(props) {
 
     function toggle() {
-        let hideContainer = document.querySelectorAll('.more-info-container');
-        Array.from(hideContainer).forEach(function(x){
-          x.classList.toggle('hidden')
-        });
+        console.log(props.character.created)
+        let hideContainer = Array.from(document.querySelectorAll('.more-info-container'));
+        hideContainer.forEach(function(x){
+            if (x.id === props.character.created) {
+                x.classList.toggle('hidden')
+            }
+        })
     }
     return (
         <div className='characterContainer'>
             <div><strong>{props.character.name}</strong></div>
             <button onClick={toggle} className='toggle'>More Info</button>
-            <div className='more-info-container hidden'>
+            <div id={props.character.created} className='more-info-container hidden'>
                 <div id='more-info' className='more-info'>
                     <strong>Year Born: </strong> {props.character.birth_year}
                 </div>
